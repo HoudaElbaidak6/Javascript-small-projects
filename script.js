55c20e7291f98c81de7b12a9ad6bc1960db26b13
Houda Elbaidak <houdaelbaidak6@gmail.com>
2022-09-07 14:04:51 +0100

    counter color

--- script.js
+++ script.js
@@ -1,17 +1,47 @@
-let output=document.getElementById("output")
-let buttonlow=document.getElementById("lowercount")
-let buttonhigh=document.getElementById("addcount")
- let count =0
+let output = document.getElementById("output");
+let buttonlow = document.getElementById("lowercount");
+let buttonhigh = document.getElementById("addcount");
+let resetbtn = document.getElementById("reset");
+let count = 0;
 
 function addnumber() {
-   output.textContent=count++;
+  output.textContent = ++count;
+  changeColor();
+}
+
+function reset() {
+  count = 0;
+  output.textContent = 0;
+  changeColor();
 }
 
 function lowernumber() {
-    output.textContent=count--;
+  output.textContent = --count;
+  changeColor();
 }
 
-buttonhigh.addEventListener("click",addnumber )
-buttonlow.addEventListener("click",lowernumber )
+function changeColor() {
+  if (output.textContent > 0) {
+    output.style.color = "red";
+  } else if (output.textContent == 0) {
+    output.style.color = "green";
+  } else {
+    output.style.color = "blue";
+  }
+}
 
+buttonhigh.addEventListener("click", addnumber);
+resetbtn.addEventListener("click", reset);
+buttonlow.addEventListener("click", lowernumber);
 
+/* let output = document.getElementById("output");
+let buttons = document.querySelectorAll("button");
+let count = Number(output.textContent);
+
+buttons[0].addEventListener("click", function () {
+  output.textContent = count--;
+});
+
+buttons[1].addEventListener("click", function () {
+  output.textContent = count++;
+}); */
