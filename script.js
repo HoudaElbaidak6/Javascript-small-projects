f6e95f064ef499bfc488675eb90790292a28dd83
Houda Elbaidak <houdaelbaidak6@gmail.com>
2022-09-07 10:22:15 +0100

    hex color done

--- script.js
+++ script.js
@@ -1,13 +1,18 @@
- let colors=["coral","pink", "purple","yellow","red","violet","lightblue","aqua"];
-let body=document.querySelector("body");
-let button=document.getElementById("btn");
 
+ let body=document.querySelector("body");
+let hexcolor=document.getElementById("hexcolor");
+let hexbtn=document.getElementById("hexbtn");
+let hextab=[0,1,2,3,4,5,6,7,8,9,"A","B","C","D","E","F"];
 
- function changeColor(event) {
-    
- let randomNumber = Math.floor(Math.random() * colors.length) ;
-body.style.backgroundColor = colors[randomNumber];
+function changehexColor(event) {
+ let hex="#"
+   for (let i = 0; i < 6; i++) {
+    let randomNumber = Math.floor(Math.random() * hextab.length) ;
+    hex+=hextab[randomNumber];
+   }
+hexcolor.textContent=hex
+body.style.backgroundColor=hex
+   
+}
 
- }
-
-button.addEventListener("click",changeColor) 
+hexbtn.addEventListener("click",changehexColor)  
