9aa74c32bb3476c815bd3b626601f4aede02962b
Houda Elbaidak <houdaelbaidak6@gmail.com>
2022-09-07 15:29:28 +0100

    bg gradient

--- script.js
+++ script.js
@@ -1,47 +1,15 @@
-let output = document.getElementById("output");
-let buttonlow = document.getElementById("lowercount");
-let buttonhigh = document.getElementById("addcount");
-let resetbtn = document.getElementById("reset");
-let count = 0;
+let body=document.querySelector("body");
+let inputs=document.querySelectorAll("input");
 
-function addnumber() {
-  output.textContent = ++count;
-  changeColor();
+function setGradient(color1,color2) {
+    document.body.style.background =`linear-gradient( to right ,${color1},${color2} ) `;
 }
 
-function reset() {
-  count = 0;
-  output.textContent = 0;
-  changeColor();
-}
+inputs[0].addEventListener("input",function () {
+    setGradient(inputs[0].value, inputs[1].value)
+    console.log(document.body.style);
+})
 
-function lowernumber() {
-  output.textContent = --count;
-  changeColor();
-}
-
-function changeColor() {
-  if (output.textContent > 0) {
-    output.style.color = "red";
-  } else if (output.textContent == 0) {
-    output.style.color = "green";
-  } else {
-    output.style.color = "blue";
-  }
-}
-
-buttonhigh.addEventListener("click", addnumber);
-resetbtn.addEventListener("click", reset);
-buttonlow.addEventListener("click", lowernumber);
-
-/* let output = document.getElementById("output");
-let buttons = document.querySelectorAll("button");
-let count = Number(output.textContent);
-
-buttons[0].addEventListener("click", function () {
-  output.textContent = count--;
-});
-
-buttons[1].addEventListener("click", function () {
-  output.textContent = count++;
-}); */
+inputs[1].addEventListener("input",function () {
+    setGradient(inputs[0].value, inputs[1].value)
+})
