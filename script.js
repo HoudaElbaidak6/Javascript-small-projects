6b41f970a8ec148df48255e882cbebbc94fc38f3
Houda Elbaidak <houdaelbaidak6@gmail.com>
2022-09-08 12:54:04 +0100

    enter key

--- script.js
+++ script.js
@@ -9,3 +9,19 @@ button.addEventListener("click", function () {
   ul.appendChild(listItem);
   input.value = "";
 });
+
+input.addEventListener("keypress", function (e) {
+  if (e.key == "Enter") {
+  if (input.value.length>0) {
+    console.log(input.value);
+    let listItem = document.createElement("li");
+    listItem.textContent = input.value;
+    let ul = document.querySelector("ul");
+    ul.appendChild(listItem);
+    input.value = "";
+  }
+  else{
+    alert("type something!")
+  }
+  }
+});
