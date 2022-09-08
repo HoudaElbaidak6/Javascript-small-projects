aff9dddc171ae35c58480e6b2f4fc2128f682a4d
Houda Elbaidak <houdaelbaidak6@gmail.com>
2022-09-08 16:31:16 +0100

    haha

--- script.js
+++ script.js
@@ -1,85 +1,31 @@
-/* let outputext, input, btn;
-outputext = document.getElementById("outputext");
-input = document.getElementById("input");
-btn = document.getElementById("btn");
+let inputkg=document.getElementById("kilo");
+let inputcm=document.getElementById("heightcm");
+let button=document.querySelector("button");
+let output=document.querySelector("#output");
 
+function BMI() {
 
-function displayName (event) {
-  let name = input.value;
-  if (name.length > 0) {
-    outputext.textContent += "  " + name;
-  }
-  input.value = "";
-}
-
-
-btn.addEventListener("click", displayName);
-
-input.addEventListener("keypress",function (event) {
-    console.log(event);
-  if (event.key == "Enter" ) {
-    displayName(event)
-  }
-  
-}) 
- */
-
-/* let colors=["coral","pink", "purple", "black","yellow","red","violet","lightblue","aqua"];
-let body=document.querySelector("body");
-let button=document.getElementById("btn");
-
-
- function changeColor(event) {
     
- let randomNumber = Math.floor(Math.random() * colors.length) ;
-body.style.backgroundColor = colors[randomNumber];
-
- }
-
-button.addEventListener("click",changeColor) */
-
-/*  let input = document.getElementById("colorInput");
-let body = document.querySelector("body");
-function changebgColor(event) {
-  body.style.backgroundColor = event.target.value;
-}
-
-input.addEventListener("input", changebgColor); */
-
-/* 
- let body=document.querySelector("body");
-let hexcolor=document.getElementById("hexcolor");
-let hexbtn=document.getElementById("hexbtn");
-let hextab=[0,1,2,3,4,5,6,7,8,9,"A","B","C","D","E","F"];
-
-function changehexColor(event) {
- let hex="#"
-   for (let i = 0; i < 6; i++) {
-    let randomNumber = Math.floor(Math.random() * hextab.length) ;
-    hex+=hextab[randomNumber];
-   }
-hexcolor.textContent=hex
-body.style.backgroundColor=hex
+    let res = (inputkg.value /(inputcm.value*inputcm)).toFixed(2);
+   console.log(res);
+    if (res<18.5) {
+        output.textContent = `${res} which means you are underweigth `;
+        console.log("underweigth");
+    }
+    else if (res>=18.5&&res<=25 ){
+        output.textContent = `${res} which means you are normal `;
+        console.log("normal");
+    }
+    else if (res>=25&&res<=30){
+        output.textContent = `${res} which means you are overweigh `;
+        console.log("overweigth");
+    }
    
 }
 
-hexbtn.addEventListener("click",changehexColor)  */
+button.addEventListener("click",BMI)
 
-/* 
-let body = document.querySelector("body");
-let color1 = document.querySelector(".color1");
-let color2 = document.querySelector(".color2");
-let gradiantinfo = document.getElementById("output");
 
-function changebgGradiant(event) {
-  body.style.background =
-    "linear-gradient" + "(" + color1.value + ", " + color2.value + ") ";
-
-  gradiantinfo.textContent = body.style.background;
-}
-
-color1.addEventListener("input", changebgGradiant);
-color2.addEventListener("input", changebgGradiant); */
 
 
 
