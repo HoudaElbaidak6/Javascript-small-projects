1e8b693ec5322622b7948dd0a8a46c6411e014b3
Houda Elbaidak <houdaelbaidak6@gmail.com>
2022-09-09 05:54:26 +0100

    done with it

--- script.js
+++ script.js
@@ -1,85 +1,33 @@
-/* let outputext, input, btn;
-outputext = document.getElementById("outputext");
-input = document.getElementById("input");
-btn = document.getElementById("btn");
+let myGuess, chance;
 
+chance = 3;
+let randomNumber = Math.floor(Math.random() * 10) + 1;
+console.log("the number was" + randomNumber);
 
-function displayName (event) {
-  let name = input.value;
-  if (name.length > 0) {
-    outputext.textContent += "  " + name;
-  }
-  input.value = "";
-}
+while (chance > 0) {
+  do {
+   myGuess = Number(prompt("Enter a number btw 1 and 10 "));
+  } while (myGuess < 1 || myGuess > 10);
 
-
-btn.addEventListener("click", displayName);
-
-input.addEventListener("keypress",function (event) {
-    console.log(event);
-  if (event.key == "Enter" ) {
-    displayName(event)
+  if (myGuess > randomNumber) {
+    alert(`${myGuess} is high ,try a lower number`);
+    console.log(`${myGuess} is high ,try a lower number`);
+    chance--;
+  } else if (myGuess == randomNumber) {
+    alert(" You got it Right !!!");
+    console.log(" You got it Right !!!");
+    // alert("You found it from " + (chance) + " essays")
+    // console.log("You found it from " + (chance) + " essays");
+    chance = 0;
+  } else {
+    alert(`${myGuess} is low ,try a higher number`);
+    console.log(`${myGuess} is low ,try a higher number`);
+    chance--;
   }
   
-}) 
- */
-
-/* let colors=["coral","pink", "purple", "black","yellow","red","violet","lightblue","aqua"];
-let body=document.querySelector("body");
-let button=document.getElementById("btn");
-
-
- function changeColor(event) {
-    
- let randomNumber = Math.floor(Math.random() * colors.length) ;
-body.style.backgroundColor = colors[randomNumber];
-
- }
-
-button.addEventListener("click",changeColor) */
-
-/*  let input = document.getElementById("colorInput");
-let body = document.querySelector("body");
-function changebgColor(event) {
-  body.style.backgroundColor = event.target.value;
-}
-
-input.addEventListener("input", changebgColor); */
-
-/* 
- let body=document.querySelector("body");
-let hexcolor=document.getElementById("hexcolor");
-let hexbtn=document.getElementById("hexbtn");
-let hextab=[0,1,2,3,4,5,6,7,8,9,"A","B","C","D","E","F"];
-
-function changehexColor(event) {
- let hex="#"
-   for (let i = 0; i < 6; i++) {
-    let randomNumber = Math.floor(Math.random() * hextab.length) ;
-    hex+=hextab[randomNumber];
-   }
-hexcolor.textContent=hex
-body.style.backgroundColor=hex
-   
-}
-
-hexbtn.addEventListener("click",changehexColor)  */
-
-/* 
-let body = document.querySelector("body");
-let color1 = document.querySelector(".color1");
-let color2 = document.querySelector(".color2");
-let gradiantinfo = document.getElementById("output");
-
-function changebgGradiant(event) {
-  body.style.background =
-    "linear-gradient" + "(" + color1.value + ", " + color2.value + ") ";
-
-  gradiantinfo.textContent = body.style.background;
-}
-
-color1.addEventListener("input", changebgGradiant);
-color2.addEventListener("input", changebgGradiant); */
+} 
+console.log("the number was" + randomNumber);
+alert("the number was" + randomNumber)
 
 
 
