c10078698a36f910411b7df5e1d9b4c44228229f
Houda Elbaidak <houdaelbaidak6@gmail.com>
2022-09-13 07:14:17 +0100

    done

--- script.js
+++ script.js
@@ -1,31 +1,63 @@
 let button = document.querySelector("button");
+let chance, randomNumber, nombretrouve;
+console.log("the number was" + randomNumber);
+let results = document.getElementById("results");
 
-function guessNumber() {
-  let yourGuess = document.querySelector("input").value;
-  let output = document.getElementById("results").value;
-  let numberReveal = document.getElementById("numberReveal").value;
+let buttonStart = document.getElementById("restart-area");
 
-  let randomNumber = Math.floor(Math.random() * 10) + 1;
-  console.log("the number was" + randomNumber);
-
-  for (let i = 0; i < 3; i++) {
-    if (yourGuess == randomNumber) {
-      output.textContent = "Your got it right  ";
-
-      console.log("Damn girl !! You got it Right !!!");
-      console.log("You found it from " + (i + 1) + " essays");
-      break;
-    } else if (yourGuess > randomNumber) {
-      output.textContent = ` ${yourGuess} is high ,try a lower number`;
-      console.log(`${yourGuess} is high ,try a lower number`);
-    } else {
-      output.textContent = `${yourGuess} is low ,try a higher number`;
-      console.log(`${yourGuess} is low ,try a higher number`);
-    }
-  }
-
-  numberReveal.textContent = `The number was ${randomNumber} `;
+function reset() {
+  chance = 3;
+  nombretrouve = false;
+  randomNumber = Math.floor(Math.random() * 10) + 1;
   console.log("the number was" + randomNumber);
 }
+function restart() {
+  let res = document.getElementById("restart-area");
+  res.style.display = "block";
+  let game = document.getElementById("game-area");
+  game.style.display = "none";
+}
+function appearGame() {
+  reset();
+  let res = document.getElementById("restart-area");
+  res.style.display = "none";
+  let game = document.getElementById("game-area");
+  game.style.display = "block";
+  results.textContent = "";
+}
 
-button.addEventListener("click", guessNumber);
+reset();
+function guessGame() {
+  let userGuess = document.querySelector("input");
+  nombreSaisie = Number(userGuess.value);
+  console.log(chance);
+
+  if (nombreSaisie > randomNumber) {
+    results.textContent = `${nombreSaisie} is high ,try a lower number`;
+    chance--;
+  } else if (nombreSaisie == randomNumber) {
+    results.textContent = "You got it right !";
+    nombretrouve = true;
+    chance = 0;
+  } else {
+    results.textContent = `${nombreSaisie} is low ,try a higher number`;
+    chance--;
+  }
+
+  if (chance == 0 && nombretrouve == false) {
+    results.textContent = "You lost booo !! ";
+    results.style.color = "black";
+    results.style.background = "red";
+    restart();
+  } else if (nombretrouve == true) {
+    results.textContent = "You won , good job !! ";
+    results.style.color = "white";
+    results.style.background = "green";
+    restart();
+  }
+
+  userGuess.value = "";
+}
+
+button.addEventListener("click", guessGame);
+buttonStart.addEventListener("click", appearGame);
