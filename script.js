10486a97cfab096defe33dae2d599da7a6ce5104
Houda Elbaidak <houdaelbaidak6@gmail.com>
2022-09-15 10:11:22 +0100

    done

--- script.js
+++ script.js
@@ -1,85 +1,78 @@
-/* let outputext, input, btn;
-outputext = document.getElementById("outputext");
-input = document.getElementById("input");
-btn = document.getElementById("btn");
+/* let success = true;
 
-
-function displayName (event) {
-  let name = input.value;
-  if (name.length > 0) {
-    outputext.textContent += "  " + name;
+let promesse = new Promise(function (resolve, reject) {
+  if (success) {
+    let a = 2;
+    let b = 7;
+    let res = a + b;
+    resolve(res);
+  } else {
+    reject("erreur de calcul");
   }
-  input.value = "";
-}
-
-
-btn.addEventListener("click", displayName);
-
-input.addEventListener("keypress",function (event) {
-    console.log(event);
-  if (event.key == "Enter" ) {
-    displayName(event)
-  }
-  
-}) 
+});
  */
 
-/* let colors=["coral","pink", "purple", "black","yellow","red","violet","lightblue","aqua"];
-let body=document.querySelector("body");
-let button=document.getElementById("btn");
-
-
- function changeColor(event) {
-    
- let randomNumber = Math.floor(Math.random() * colors.length) ;
-body.style.backgroundColor = colors[randomNumber];
-
- }
-
-button.addEventListener("click",changeColor) */
-
-/*  let input = document.getElementById("colorInput");
-let body = document.querySelector("body");
-function changebgColor(event) {
-  body.style.backgroundColor = event.target.value;
+/* function NombreEntier(nbre) {
+  let promesse = new Promise(function (resolve, reject) {
+    if (nbre % 2 == 0) {
+      res = nbre * nbre;
+      resolve(res);
+    } else {
+      reject("erreur : Nombre impaire");
+    }
+  });
+  return promesse;
 }
 
-input.addEventListener("input", changebgColor); */
+let prom = NombreEntier(4);
+prom
+  .then((resultat) => {
+    console.log(resultat);
+  })
+  .catch((erreur) => {
+    console.log(erreur);
+  });
+ */
 
-/* 
- let body=document.querySelector("body");
-let hexcolor=document.getElementById("hexcolor");
-let hexbtn=document.getElementById("hexbtn");
-let hextab=[0,1,2,3,4,5,6,7,8,9,"A","B","C","D","E","F"];
-
-function changehexColor(event) {
- let hex="#"
-   for (let i = 0; i < 6; i++) {
-    let randomNumber = Math.floor(Math.random() * hextab.length) ;
-    hex+=hextab[randomNumber];
-   }
-hexcolor.textContent=hex
-body.style.backgroundColor=hex
-   
+/* function sommeTab(n) {
+  let tab = [2, 10, 30, 4, -1, 0];
+  let somme = 0;
+  let promesse = new Promise(function (resolve, reject) {
+    if (n <= tab.length && n > 0) {
+      for (let i = 0; i < tab.length; i++) {
+        somme = somme + tab[i];
+      }
+      resolve(somme);
+    } else {
+      reject("Index of Array out of bounds");
+    }
+  });
+  return promesse;
 }
 
-hexbtn.addEventListener("click",changehexColor)  */
+let prom = sommeTab(4);
+prom
+  .then(function (res) {
+    console.log(res);
+  })
+  .catch(function (res) {
+    console.log(res);
+  }); */
 
-/* 
-let body = document.querySelector("body");
-let color1 = document.querySelector(".color1");
-let color2 = document.querySelector(".color2");
-let gradiantinfo = document.getElementById("output");
+let promesse = new Promise(function (resolve, reject) {
+  let phrase = " I hate Bissara ";
+  if (phrase.length > 5) {
+    let regex = /(\w+) (\w+) (\w+)/;
+    resolve(phrase.replace(regex, "$3 $2 $1"));
+  } else {
+    reject("Phrase incorrect");
+  }
+});
 
-function changebgGradiant(event) {
-  body.style.background =
-    "linear-gradient" + "(" + color1.value + ", " + color2.value + ") ";
-
-  gradiantinfo.textContent = body.style.background;
-}
-
-color1.addEventListener("input", changebgGradiant);
-color2.addEventListener("input", changebgGradiant); */
-
-
-
+promesse
+  .then(function (res) {
+    console.log(res);
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
